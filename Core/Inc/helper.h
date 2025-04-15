a7dff5fbd2eedd3dd5f70f013333a7e4ef4c7043
mikaelmarvin <mikael.marvin@porsche-ep.com>
2025-04-15 15:54:32 +0200

    initial commit

---
+++ Core/Inc/helper.h
@@ -0,0 +1,18 @@
+/*
+ * helper_functions.h
+ *
+ *  Created on: Mar 27, 2025
+ *      Author: mikaelmarvin
+ */
+
+#ifndef INC_HELPER_H_
+#define INC_HELPER_H_
+
+#include "i2c_request.h"
+#include "imu_sensor.h"
+
+
+
+const char *i2c_op_to_string(i2c_op_type_t op);
+
+#endif /* INC_HELPER_H_ */
---
+++ Core/Inc/i2c_request.h
@@ -0,0 +1,54 @@
+/*
+ * i2c_comm_it.h
+ *
+ *  Created on: Mar 22, 2025
+ *      Author: Mikael Marvin
+ */
+
+#ifndef INC_I2C_REQUEST_H_
+#define INC_I2C_REQUEST_H_
+
+#include "stm32f4xx_hal.h"
+#include "FreeRTOS.h"
+#include "queue.h"
+
+#define I2C_QUEUE_SIZE 32
+
+// Enum for supported I2C operations
+typedef enum {
+    I2C_OP_MEM_WRITE,
+    I2C_OP_MEM_READ,
+    I2C_OP_MASTER_TRANSMIT,
+    I2C_OP_SLAVE_TRANSMIT,
+    I2C_OP_MASTER_RECEIVE,
+    I2C_OP_SLAVE_RECEIVE
+} i2c_op_type_t;
+
+typedef struct {
+    uint8_t dev_addr;
+    uint8_t reg_addr;
+    uint8_t *tx_buf;
+    uint8_t *rx_buf;
+    uint16_t len;
+    void *user_data;  // pointer to struct like ImuSensor_t
+    void (*parse_fn)(uint8_t *rx_buf, void *user_data);
+    i2c_op_type_t op_type;
+    union {
+        HAL_StatusTypeDef (*mem_read)(I2C_HandleTypeDef *i2c, uint8_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size);
+        HAL_StatusTypeDef (*mem_write)(I2C_HandleTypeDef *i2c, uint8_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size);
+        HAL_StatusTypeDef (*master_transmit)(I2C_HandleTypeDef *i2c, uint8_t DevAddress, uint8_t *pData, uint16_t Size);
+        HAL_StatusTypeDef (*master_receive)(I2C_HandleTypeDef *i2c, uint8_t DevAddress, uint8_t *pData, uint16_t Size);
+    } i2c_call; // This will hold specific function pointers for each I2C operation
+} i2c_request_t;
+
+/*
+ * Initialize the i2c queue that handles interrupt based i2c requests
+ */
+void i2c_request_queue_init();
+
+/*
+ * Add request to the i2c queue
+ */
+HAL_StatusTypeDef i2c_submit_request(I2C_HandleTypeDef *i2c, i2c_request_t *req);
+
+#endif /* INC_I2C_REQUEST_H_ */
